a253fcab4dfe76ac5b0866aeb3f9051c136f78f0
seabelle mcfarlane <seabellemcfarlane@aol.com>
2018-01-22 20:19:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+var chocolateBars = ["Snickers", "hundred grand", "kickcat", "skittles"]
 fuction addElementToBeginningOfArray(array, element){
   var new_array = [element, array[0]]
   return new_array
